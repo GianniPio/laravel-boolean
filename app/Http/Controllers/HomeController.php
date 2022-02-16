50d9b4046b438411fcfbf34e296bc01ab067d307
GianniPio <giannipiopellegrino00@gmail.com>
2022-02-16 09:26:31 +0100

    install auth for vue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function home() {
-
-        return view('pages.home');
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', 'HomeController@home') ->name('home');
+// Route::get('/home', 'HomeController@home') ->name('home');
+
+// Auth::routes();
+
+Route::get('/home', 'GuestController@home')->name('home');
